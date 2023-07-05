ee4acbe3dd7bbb019e802dc70129c2fed23d382b
Lê Hoàng Anh <anhlh@synodus.com>
2023-07-05 18:41:05 +0700

    fix missing import functions in hashingHelper

--- src/hashingHelper.js
+++ src/hashingHelper.js
@@ -1,31 +1,31 @@
 const { utils } = require("ethers");
-const {
-    pack,
-    stringToBytes,
-    hexToString,
-    toBytes10Name,
-} = require("./encodingHelper");
+const { encodingHelper } = require("./encodingHelper");
 
 function toRoleHash(address, name) {
     return utils.keccak256(
-        pack(["address", "string"], [utils.getAddress(address), name])
+        encodingHelper.pack(
+            ["address", "string"],
+            [utils.getAddress(address), name]
+        )
     );
 }
 
 function toBytes32Key(keyString) {
-    return stringToBytes(keyString, 32);
+    return encodingHelper.stringToBytes(keyString, 32);
 }
 
 function toBytes32Value(nameString, index, address) {
-    return pack(
+    return encodingHelper.pack(
         ["bytes10", "uint16", "address"],
-        [toBytes10Name(nameString), index, address]
+        [encodingHelper.toBytes10Name(nameString), index, address]
     );
 }
 
 function recoverOriginalValues(bytes32HexValue) {
     let bytesArrayValue = utils.arrayify(bytes32HexValue);
-    let nameString = hexToString(utils.hexlify(bytesArrayValue.slice(0, 10)));
+    let nameString = encodingHelper.hexToString(
+        utils.hexlify(bytesArrayValue.slice(0, 10))
+    );
     let index = parseInt(utils.hexlify(bytesArrayValue.slice(10, 12)));
     let address = utils.getAddress(
         utils.hexlify(bytesArrayValue.slice(12, 32))
